3fd8512b1bc090f35812032ba72efa4f72fb4c0e
Ayush Nainwal <iayush.n2@gmail.com>
2023-08-21 11:16:22 +0530

    Initial Commit_

---
+++ modern_website_react/src/App.jsx
@@ -0,0 +1,40 @@
+import React from 'react'
+import styles from './style'
+import {Navbar,Hero,Stats,Business,Billing,CardDeal,Testimonials,Clients,CTA,Footer,Button} from "./Components"
+import { useState } from 'react'
+
+//run rafce + enter
+
+const App = () => {
+  return (
+    <div className='bg-primary w-full overflow-hidden'>
+      <div className={`${styles.paddingX} ${styles.flexCenter}`}>
+        <div className={`${styles.boxWidth}`}>
+          <Navbar/>
+        </div>
+      </div>
+
+      <div className={`bg-primary ${styles.flexStart}`}>
+        <div className={`${styles.boxWidth}`}>
+          <Hero/>
+        </div>
+      </div>
+
+      <div className={`bg-primary ${styles.paddingX} ${styles.flexStart}`}>
+        <div className={`${styles.boxWidth}`}>
+          <Stats/>
+          <Business/>
+          <Billing/>
+          <CardDeal/>
+          <Testimonials/>
+          <Clients/>
+          <CTA/>
+          <Footer/>
+        </div>
+      </div>
+
+    </div>
+  )
+}
+
+export default App
